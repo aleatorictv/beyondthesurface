f8700962f395ba33afcae9575a815e5458265243
JS <aleatorictelevision@gmail.com>
2017-07-07 00:38:34 -0400

    trying to make groupings

--- js/main.js
+++ js/main.js
@@ -49,85 +49,11 @@
 	wall3 = new THREE.Group();
 	wall4 = new THREE.Group();
 	wall5 = new THREE.Group();
-	
-    _wall1 = [];
-    for (var i=0;i<7;i++){
-      var ii= ((i%2==0)? 4 : 5);
-      for(var j=0;j<ii;j++){
-       if( ii==4)  _wall1.push(new tile(0, 30+i*15, j*30,0));
-       else  _wall1.push(new tile(0, 30+i*15, -15+j*30,0));
-      }
-    }
-    for(var i=0;i<_wall1.length;i++){
-       wall1.add(_wall1[i].tile);
-    }    
-	
-	wall1.position.set(200,0,-30);	
-	scene.add(wall1);
-	
-	_wall2 = [];
-    for (var i=0;i<7;i++){
-      var ii= ((i%2==0)? 4 : 5);
-      for(var j=0;j<ii;j++){
-       if( ii==4)  _wall2.push(new tile(0, 30+i*15, j*30,0));
-       else  _wall2.push(new tile(0, 30+i*15, -15+j*30,0));
-      }
-    }
-    for(var i=0;i<_wall2.length;i++){
-       wall2.add(_wall2[i].tile);
-    }
-	wall2.rotateY(1.57);
-	wall2.position.set(-60,0,-100);
-	scene.add(wall2);
-	
-	_wall3 = [];
-    for (var i=0;i<7;i++){
-      var ii= ((i%2==0)? 4 : 5);
-      for(var j=0;j<ii;j++){
-       if( ii==4)  _wall3.push(new tile(0, 30+i*15, j*30,0));
-       else  _wall3.push(new tile(0, 30+i*15, -15+j*30,0));
-      }
-    }
-    for(var i=0;i<_wall3.length;i++){
-       wall3.add(_wall3[i].tile);
-    }
-	wall3.rotateY(3.14);
-	wall3.position.set(-100,0,170);
-	scene.add(wall3);
 
-	wall4 = [];
-    for (var i=0;i<7;i++){
-      var ii= ((i%2==0)? 1 : 2);
-      for(var j=0;j<ii;j++){
-       if( ii==1	)  wall4.push(new tile(125,  30+i*15, 15+ j*30,3));
-       else  		wall4.push(new tile(125,     30+i*15, 0+j*30,3));
-      }
-    }
-    for (var i=0;i<7;i++){
-      var ii= ((i%2==0)? 1 : 2);
-      for(var j=0;j<ii;j++){
-       if( ii==1	)  wall4.push(new tile(125,  30+i*15, -75+j*30,3));
-       else  		wall4.push(new tile(125, 30+i*15, -90+j*30,3));
-      }
-    }
-    for(var i=0;i<wall4.length;i++){
-      scene.add(wall4[i].tile);
-    }	
-	_wall5 = [];
-    for (var i=0;i<11;i++){
-      var ii= ((i%2==0)? 3 : 4);
-      for(var j=0;j<ii;j++){
-       if( ii==3)  _wall5.push(new tile(0, 30+i*15, j*36,0));
-       else  _wall5.push(new tile(0, 	30+i*15, -18+j*36,0));
-      }
-    }
-    for(var i=0;i<_wall5.length;i++){
-       wall5.add(_wall5[i].tile);
-    }
-	wall5.rotateZ(1.57);
-	wall5.rotateX(1.57);
-	wall5.position.set(-30,275,-150);
-	scene.add(wall5);
+  buildwalls();
+
+  var walls = [wall1,wall2,wall3,wall4,wall5];
+
   var flmaterial = new THREE.MeshBasicMaterial( {wireframe: false, color: 0x777777,side: THREE.DoubleSide} );
   var floor = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000,10,10), flmaterial);
 
@@ -135,15 +61,15 @@
   var controls = new THREE.OrbitControls( camera, renderer.domElement );
   scene.add(floor);
 
-
-    // Finally, add the sphere to the scene.
-    //scene.add(sphere);
-
+for(tile t in wall1){
+    t.changeColor(0xFFFFFF);
+}
     function update () {
       // Draw!
       renderer.render(scene, camera);
       controls.update();
-	  console.log(camera.position);
+
+	  //console.log(camera.position);
       // Schedule the next frame.
       requestAnimationFrame(update);
     }
--- js/tile.js
+++ js/tile.js
@@ -10,41 +10,43 @@ function tile(x,y,z,w){
 	this.cube = new THREE.Mesh(this.geometry, this.material );
 	this.ledPos = [];
 	this.leds = [];
-
+	this.left = [];
+	this.top = [];
+	this.right = [];
+	this.bottom = [];
 	this.allLeds = new THREE.Group();
 	this.tile = new THREE.Group();
-	
+
 	for (var i=0;i<8;i++){
 		this.ledPos.push(new Array(this.x,this.y+i*3, this.z));
-		this.leds.push(new THREE.Mesh(this.geometry, this.material ) );
 	}
 	for (var i=0;i<7;i++){
 		this.ledPos.push(new Array(this.x,this.y+24,this.z+ i*3+3));
-		this.leds.push(new THREE.Mesh(this.geometry, this.material ) );
 	}
 	for (var i=0;i<8;i++){
 		this.ledPos.push(new Array(this.x,this.y+21-i*3, this.z+24));
-		this.leds.push(new THREE.Mesh(this.geometry, this.material ) );
 	}
 	for (var i=0;i<7;i++){
 		this.ledPos.push(new Array(this.x,this.y-3,this.z+i*3+3));
-		this.leds.push(new THREE.Mesh(this.geometry, this.material ) );
 	}
-	this.setlights();
+	this.setpositions();
+}
+tile.prototype.changeColor = function(c){
+	this.allLeds.material.color.setHex(c);
 }
 
-tile.prototype.setlights = function(){
+tile.prototype.setpositions = function(){
 	for(var i =0;i<30;i++){
+		this.leds.push(new THREE.Mesh(this.geometry, this.material ) );
 		this.leds[i].position.set( this.ledPos[i][0], this.ledPos[i][1], this.ledPos[i][2]);
 		this.allLeds.add(this.leds[i]);
 	}
 	this.tile.add(this.allLeds);
 	this.allLeds.position.set(-.5,-12,-.5);
 	this.allLeds.position.set(this.x,this.y,this.z);
-	
+
 	if(this.w==1) this.tile.rotateY(1.57);
 	else if(this.w==2) this.tile.rotateY(3.14);
 	else if(this.w==3) this.tile.rotateY(-1.57);
 	else if(this.w==4) this.tile.rotateZ(1.57);
 }
-
