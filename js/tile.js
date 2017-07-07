fb06616f501bbb44429078efb382e7638370d57f
James Sannino <aleatorictelevision@gmail.com>
2017-07-06 20:43:33 -0400

    walls are up

--- js/tile.js
+++ js/tile.js
@@ -12,48 +12,39 @@ function tile(x,y,z,w){
 	this.leds = [];
 
 	this.allLeds = new THREE.Group();
-
+	this.tile = new THREE.Group();
+	
 	for (var i=0;i<8;i++){
 		this.ledPos.push(new Array(this.x,this.y+i*3, this.z));
 		this.leds.push(new THREE.Mesh(this.geometry, this.material ) );
 	}
 	for (var i=0;i<7;i++){
-		this.ledPos.push(new Array(this.x,this.y+24, this.z+i*3+3));
+		this.ledPos.push(new Array(this.x,this.y+24,this.z+ i*3+3));
 		this.leds.push(new THREE.Mesh(this.geometry, this.material ) );
 	}
 	for (var i=0;i<8;i++){
-		this.ledPos.push(new Array(this.x,this.y+21- i*3, this.z + 24));
+		this.ledPos.push(new Array(this.x,this.y+21-i*3, this.z+24));
 		this.leds.push(new THREE.Mesh(this.geometry, this.material ) );
 	}
 	for (var i=0;i<7;i++){
-		this.ledPos.push(new Array(this.x,this.y-3, this.z + i*3+3));
+		this.ledPos.push(new Array(this.x,this.y-3,this.z+i*3+3));
 		this.leds.push(new THREE.Mesh(this.geometry, this.material ) );
 	}
 	this.setlights();
-	this.xyz(this.x,this.y,this.z);
-	if(w==1) this.allLeds.rotateY(1.57);
-	if(w==2) this.allLeds.rotateY(3.14);
-	if(w==3) this.allLeds.rotateY(-1.57);
-	if(w==4) this.allLeds.rotateZ(1.57);
 }
 
 tile.prototype.setlights = function(){
 	for(var i =0;i<30;i++){
-		this.leds[i].position.x = this.ledPos[i][0];
-		this.leds[i].position.y = this.ledPos[i][1];
-		this.leds[i].position.z = this.ledPos[i][2];
+		this.leds[i].position.set( this.ledPos[i][0], this.ledPos[i][1], this.ledPos[i][2]);
 		this.allLeds.add(this.leds[i]);
 	}
+	this.tile.add(this.allLeds);
+	this.allLeds.position.set(-.5,-12,-.5);
+	this.allLeds.position.set(this.x,this.y,this.z);
+	
+	if(this.w==1) this.tile.rotateY(1.57);
+	else if(this.w==2) this.tile.rotateY(3.14);
+	else if(this.w==3) this.tile.rotateY(-1.57);
+	else if(this.w==4) this.tile.rotateZ(1.57);
 }
 
-tile.prototype.xyz = function(x,y,z){
-	this.x=x;
-	this.y=y;
-	this.z=z;
-	this.allLeds.position.x = x;
-	this.allLeds.position.y = y;
-	this.allLeds.position.z = z;
-}
-tile.prototype.turn= function(v){
-
-}
